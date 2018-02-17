6814c68f5fa58e4b244c531c788d099fa5e92c28
Steelmaker86 <Steelmaker86@gmail.com>
2018-02-17 14:05:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ client.on('message', message => {
  if (command===prefix + "servers" || command===prefix + "guilds")
    if (message.author.id === "212589934966472704")
      if (client.guilds.size != 1)
-       message.author.send("I am in " + client.guilds.size + " servers."
+       message.author.send("I am in " + client.guilds.size + " servers.");
      if (client.guilds.size ===1)
       message.author.send("I am in 1 server.");
        
