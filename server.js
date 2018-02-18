73a9fc546a277e8ec83036ed2b1c3d5c6528a710
Steelmaker86 <Steelmaker86@gmail.com>
2018-02-17 18:11:44 -0600

    Update server.js

--- server.js
+++ server.js
@@ -37,13 +37,11 @@ client.on('message', message => {
  if (command===prefix + "dmtest")
     message.author.send("success!");
  if (command===prefix + "echo")
-   if (message.author.id == "393832882637897729")
+   if (message.author.id != "393832882637897729")
      message.channel.send(par);
  if (command===prefix + "hello" || command === prefix + "hi")
      message.channel.send("Hello <@" + message.author.id + ">, how are you doing?")
- if (command==prefix + "invite" && command === prefix + "invite")
-   message.author.send("If you would like to use me, ask <@212589934966472704> for permission, and join https://discord.gg/GZNcjhy for support and updates. Thank you for reading.")
-   message.channel.send("Check your DMs for information.");
+   
        
 });
 // Log our bot in
