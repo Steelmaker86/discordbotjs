8d493057e3de49349211e926f957ffd003ea17ee
Steelmaker86 <Steelmaker86@gmail.com>
2018-02-19 13:15:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 /*
   A bot that welcomes new guild members when they join
 */
+const Stopwatch = require("node-stopwatch").Stopwatch;
 
+
+const stopwatch = Stopwatch.create();
 // Import the discord.js module
 const Discord = require('discord.js');
 // Create an instance of a Discord client
@@ -48,6 +51,10 @@ client.on('message', message => {
      message.channel.send("Hello <@" + message.author.id + ">, how are you doing?");
    if (command===prefix + "invite")
      message.channel.send("Currently, I am in development, if you would like to invite me, please ask <@212589934966472704> for permission.");
+   if (command===prefix + "ping")
+     stopwatch.start();
+     message.channel.send("Pong! It took " + stopwatch.elapsedMilliseconds + "ms.");
+     stopwatch.stop();
 });
 // Log our bot in
 client.login(process.env.SECRET);
