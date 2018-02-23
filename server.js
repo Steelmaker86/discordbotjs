f97b99982a8d76187597003ecf64824dd93e1f8a
Steelmaker86 <Steelmaker86@gmail.com>
2018-02-23 17:05:09 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
+const rich = new Discord.RichEmbed();
 // The token of your bot - https://discordapp.com/developers/applications/me
 const prefix = '/'
 const Stopwatch = require("node-stopwatch").Stopwatch;
@@ -49,6 +50,13 @@ client.on('message', message => {
      message.channel.send("Hello <@" + message.author.id + ">, how are you doing?");
    if (command===prefix + "invite")
      message.channel.send("Currently, I am in development, if you would like to invite me, please ask <@212589934966472704> for permission.");
+   if (command===prefix + "meme")
+     var m = par.split(" ")
+     rich.setColor(#e60000)
+     rich.setImage("http://urlme.me/" + m[0] + "/" + m[1] + "/" + m[2])
+     message.channel.send({embed});
+  
+     
 });
 // Log our bot in
 client.login(process.env.SECRET);
