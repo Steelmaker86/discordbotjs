77162f4073d15338d8605d62c4b54fdfa8cb0e29
Steelmaker86 <Steelmaker86@gmail.com>
2018-03-10 12:01:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const client = new Discord.Client();
 const prefix = '/'
 const Stopwatch = require("node-stopwatch").Stopwatch;
 const stopwatch = Stopwatch.create();
-const jokes = ["Why do Java programmers wear glasses? Because they can't C#!", "What did the experienced programmer say to the ignorant one? Git good. (I wanna kill myself)"];
+const jokes = ["Why do Java programmers wear glasses? Because they can't C#!", "What did the experienced programmer say to the ignorant one? Git good."];
 
 client.on('ready', () => {
   if(client.guilds.size >= 20)
