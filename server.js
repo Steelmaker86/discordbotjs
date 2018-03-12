1337360093c2c0a07e7c89f710ea6fb32a89f485
Steelmaker86 <Steelmaker86@gmail.com>
2018-03-12 16:32:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 // The token of your bot - https://discordapp.com/developers/applications/me
 const prefix = '/'
-const Stopwatch = require("node-stopwatch").Stopwatch;
-const stopwatch = Stopwatch.create();
+
 
 client.on('ready', () => {
   if(client.guilds.size >= 20)
@@ -28,7 +27,7 @@ client.on('guildMemberAdd', member => {
 });
 client.on('message', message => {
   if (message.author.bot)
-    return
+    break
   else
     var str = message.content.indexOf(" ")
     var command = message.content.split(" ")[0]
@@ -49,8 +48,6 @@ client.on('message', message => {
      message.channel.send("Hello <@" + message.author.id + ">, how are you doing?");
    if (command===prefix + "invite")
      message.channel.send("Currently, I am in development, if you would like to invite me, please ask <@212589934966472704> for permission.");
-  
-
 });
 // Log our bot in
 client.login(process.env.SECRET);
