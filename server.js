e05a0ac61d51b0c5f8693396d9aec83067a75a30
Steelmaker86 <Steelmaker86@gmail.com>
2018-03-24 12:51:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const prefix = '/'
 
 
 client.on('ready', () => {
-  client.user.setStatus('dnd')
+  client.user.setStatus('dnd');
   hook.send("My body is reggie");
   if(client.guilds.size >= 20)
   console.log('Bot is online in a whopping ' + client.guilds.size + ' servers!')
