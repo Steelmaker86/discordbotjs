488e9b71f69fcbb931b3a054e337610ba45c6ee4
Steelmaker86 <Steelmaker86@gmail.com>
2018-03-24 14:21:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,14 @@
 const Discord = require('discord.js');
 
 // Create an instance of 
+const Music = require('discord.js-musicbot-addon');
 const client = new Discord.Client(); 
 const hook = new Discord.WebhookClient("426893085356720129", process.env.TOKEN);
 // The token of your bot - https://discordapp.com/developers/applications/me
 const prefix = '/'
+const music = new Music(client, {
+  youtubeKey: process.env.KEY
+});
 
 
 client.on('ready', () => {
