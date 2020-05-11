e369348f01c3a47ab354b82c8ebb2e39193522e2
steelmaker86 <Steelmaker86@gmail.com>
2020-05-11 14:33:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,12 @@
 const Discord = require('discord.js');
 
 // Create an instance of 
-const Music = require('discord.js-musicbot-addon');
 const client = new Discord.Client(); 
-const hook = new Discord.WebhookClient("426893085356720129", process.env.TOKEN);
 // The token of your bot - https://discordapp.com/developers/applications/me
 const prefix = '/'
-const music = new Music(client, {
-  ownerOverMember: true,
-  botOwner: '212589934966472704',
-  prefix: '/',
-  youtubeKey: process.env.KEY,
-  botAdmins: ['212589934966472704', '319139711836880896'],
-  enableQueueStat: true
-});
 
 
 client.on('ready', () => {
-  client.user.setPresence({ activity: { name: 'Jab Battles' }, status: 'dnd' });
-  hook.send("My body is reggie");
   if(client.guilds.size >= 20)
   console.log('Bot is online in a whopping ' + client.guilds.size + ' servers!')
    if(client.guilds.size===1)
